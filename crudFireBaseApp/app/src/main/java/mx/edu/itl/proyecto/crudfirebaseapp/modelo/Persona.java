485bdb4e387a0be830391a38c312fa57fef301dc
Roberto Mendez <82856059+DevilsKind98@users.noreply.github.com>
2021-12-04 18:50:29 -0600

    modelo

---
+++ crudFireBaseApp/app/src/main/java/mx/edu/itl/proyecto/crudfirebaseapp/modelo/Persona.java
@@ -0,0 +1,44 @@
+package mx.edu.itl.proyecto.crudfirebaseapp.modelo;
+
+public class Persona {
+
+    public String uid;
+    public String Nombre;
+    public String Apellidos;
+    public String Correo;
+
+    public Persona() {
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        Apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+}
